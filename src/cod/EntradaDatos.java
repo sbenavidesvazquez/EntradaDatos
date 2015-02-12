9c695f90a923dc45b9810a50774774db48b692df
sbenavidesvazquez <sbenavidesvazquez@eniac20.danielcastelao.org>
2015-02-12 09:20:17 +0100

    Introducir

--- src/cod/EntradaDatos.java
+++ src/cod/EntradaDatos.java
@@ -9,16 +9,14 @@ public class EntradaDatos {
     
     public int introducir(int num){
         Scanner dato=new Scanner(System.in);
-        int op;
-        op=Integer.parseInt(JOptionPane.showInputDialog("*******MENU*****\n")
-                                                        +"0 para introducir por pantalla\n"
-                                                        +"1 para introducir en cuadrado");        
-        switch(op){
-            case 0: num=dato.nextInt();
-                System.out.println(num);
-            case 1: num=Integer.parseInt(JOptionPane.showInputDialog("Introduce un número"));
-                JOptionPane.showMessageDialog(null, num);
+        int op=0;
+               
+        switch(num){
+            case 0: op=dato.nextInt();
+                System.out.println(op);
+            case 1: op=Integer.parseInt(JOptionPane.showInputDialog("Introduce un número"));
+                JOptionPane.showMessageDialog(null, op);
         }
-        return num;
+        return op;
     }
 }
