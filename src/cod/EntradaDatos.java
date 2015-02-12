4572c12d200bdcf378156bfdf9306ec6b117cea0
sbenavidesvazquez <sbenavidesvazquez@eniac20.danielcastelao.org>
2015-02-12 09:40:14 +0100

    Introducir 3

--- src/cod/EntradaDatos.java
+++ src/cod/EntradaDatos.java
@@ -11,20 +11,23 @@ import javax.swing.JOptionPane;
 
 public class EntradaDatos {
     /**
-     * si num=0, introduces por pantalla, si numn=1, introduces por cuadrado.
-     * @param num
-     * @return op
+     * introducir número por pantalla o por cuadrado.
+     * @param num si num=0, introduces por pantalla, si numn=1, introduces por cuadrado.
+     * @return op devuelve un número.
      */
-    public int introducir(int num){
+    public static int introducir(int num){
         Scanner dato=new Scanner(System.in);
         int op=0;
                
         switch(num){
             case 0: op=dato.nextInt();
                 System.out.println(op);
+                break;
             case 1: op=Integer.parseInt(JOptionPane.showInputDialog("Introduce un número"));
                 JOptionPane.showMessageDialog(null, op);
+                break;
             default: op=0;
+                break;
                        
         }
         return op;
