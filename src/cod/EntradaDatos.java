b36f7d8dd92b2eb3bac05cd11094d6f5b4a85947
sbenavidesvazquez <sbenavidesvazquez@eniac20.danielcastelao.org>
2015-02-12 09:29:13 +0100

    Introducir 1

--- src/cod/EntradaDatos.java
+++ src/cod/EntradaDatos.java
@@ -3,10 +3,18 @@ package cod;
 
 import java.util.Scanner;
 import javax.swing.JOptionPane;
-
+/**
+ * Esta clase es una librería para subir a github en el cual te hara introducir por pantalla o por cuadrado.
+ * @author sbenavidesvazquez
+ * @version 0.1
+ */
 
 public class EntradaDatos {
-    
+    /**
+     * 
+     * @param num
+     * @return op
+     */
     public int introducir(int num){
         Scanner dato=new Scanner(System.in);
         int op=0;
@@ -16,6 +24,8 @@ public class EntradaDatos {
                 System.out.println(op);
             case 1: op=Integer.parseInt(JOptionPane.showInputDialog("Introduce un número"));
                 JOptionPane.showMessageDialog(null, op);
+            default: op=0;
+                       
         }
         return op;
     }
